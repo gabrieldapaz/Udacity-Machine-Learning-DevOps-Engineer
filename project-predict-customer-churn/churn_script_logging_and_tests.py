d828ea9118860613bbf6151f2f220f3c5bb8f2ff
Gabriel da Paz <dpzgabriel@gmail.com>
2022-11-13 17:34:44 -0300

    create a CustomerChurn class to wrap up the test functions. instanciate the CustomerChurn class on test functions. Develop test_eda

--- project-predict-customer-churn/churn_script_logging_and_tests.py
+++ project-predict-customer-churn/churn_script_logging_and_tests.py
@@ -1,6 +1,7 @@
 import os
 import logging
-import churn_library_solution as cls
+from churn_library import CustomerChurn
+import pytest
 
 logging.basicConfig(
     filename='./logs/churn_library.log',
@@ -8,47 +9,62 @@ logging.basicConfig(
     filemode='w',
     format='%(name)s - %(levelname)s - %(message)s')
 
-def test_import(import_data):
-	'''
-	test data import - this example is completed for you to assist with the other test functions
-	'''
-	try:
-		df = import_data("./data/bank_data.csv")
-		logging.info("Testing import_data: SUCCESS")
-	except FileNotFoundError as err:
-		logging.error("Testing import_eda: The file wasn't found")
-		raise err
+class TestCustomerChurn:
 
-	try:
-		assert df.shape[0] > 0
-		assert df.shape[1] > 0
-	except AssertionError as err:
-		logging.error("Testing import_data: The file doesn't appear to have rows and columns")
-		raise err
+    def test_import(self):
+        '''
+        test data import - this example is completed for you to assist with the other test functions
+        '''
+        try:
+            cc = CustomerChurn()
+            df = cc.import_data("./data/bank_data.csv")
+            pytest.df = df
+            logging.info("Testing import_data: SUCCESS")
+        except FileNotFoundError as err:
+            logging.error("Testing import_eda: The file wasn't found")
+            raise err
 
-
-def test_eda(perform_eda):
-	'''
-	test perform eda function
-	'''
+        try:
+            assert df.shape[0] > 0
+            assert df.shape[1] > 0
+        except AssertionError as err:
+            logging.error("Testing import_data: The file doesn't appear to have rows and columns")
+            raise err
 
 
-def test_encoder_helper(encoder_helper):
-	'''
-	test encoder helper
-	'''
+    def test_eda(self):
+        '''
+        test perform eda function
+        '''
+        try:
+            cc = CustomerChurn()
+            cc.perform_eda(pytest.df)
+
+            cwd = os.path.abspath(os.getcwd())
+            assert len(os.listdir(cwd+'/images/eda/')) == 4
+            logging.info("Testing perform_eda: SUCCESS")
+        except FileNotFoundError as err:
+            logging.error("Testing perform_eda: The directory doesn't appear to exist")
+            raise err
 
 
-def test_perform_feature_engineering(perform_feature_engineering):
-	'''
-	test perform_feature_engineering
-	'''
+
+    def test_encoder_helper(encoder_helper):
+        '''
+        test encoder helper
+        '''
 
 
-def test_train_models(train_models):
-	'''
-	test train_models
-	'''
+    def test_perform_feature_engineering(perform_feature_engineering):
+        '''
+        test perform_feature_engineering
+        '''
+
+
+    def test_train_models(train_models):
+        '''
+        test train_models
+        '''
 
 
 if __name__ == "__main__":
